240a9179bf7d787fdc7ed06ebc6f1f920ed9c035
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-01-26 22:00:49 +0300

    Reformat Code

--- src/IndexService.java
+++ src/IndexService.java
@@ -1,7 +1,7 @@
 public class IndexService {
     public double calculate(double height_sm, double weight_kg) {
         double index;
-        index = weight_kg /( (height_sm * height_sm) / 10000);
+        index = weight_kg / ((height_sm * height_sm) / 10000);
         return index;
     }
 }
